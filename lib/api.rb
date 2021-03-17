0afe1a37ee25935d2db487e35a79623665ffef06
Collins Tatang <makungongcollins@gmail.com>
2021-03-17 10:43:15 +0100

    API client Object created in the api.rb file

---
+++ lib/api.rb
@@ -0,0 +1,10 @@
+class twitterAPI
+    def initialize
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key = 'API-Key'
+        config.consumer_secret = 'API-Key secret'
+        config.access_token = 'Access Token'
+        config.access_token_secret = 'Access Token Secret'
+      end
+    end
+end 
