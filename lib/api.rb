11a99f927af901660d55b95c9c93cefc0268c159
Collins Tatang <makungongcollins@gmail.com>
2021-03-19 15:51:10 +0100

    Created an agent instance variable in the bot.rb file and an API keys class in the api.rb file

--- lib/api.rb
+++ lib/api.rb
@@ -6,10 +6,10 @@ class API
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = 'jBqrJVcho0rrS8Y2Jdy0OmMN8'
-      config.consumer_secret = '9cKtoD394bqoSrc51VSeKEC6HmZzBiVwek09qyoG9vPDe7zdjL'
-      config.access_token = '1368890304586203136-92Al1bHjOVuKmpUvx1k0tJ8jFa5Ngu'
-      config.access_token_secret = 'W8WQ806TbrUtocG0lcJ2571SAw2vTmbt8AkHqjbh3rLUp'
+      config.consumer_key = 'API-Key'
+      config.consumer_secret = 'API-Key secret'
+      config.access_token = 'Access Token'
+      config.access_token_secret = 'Access Token Secret'
     end
-  end
+end
 end
