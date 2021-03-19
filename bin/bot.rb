cc945258f51105f3290b49c182cf8a1568deb5e0
Collins Tatang <makungongcollins@gmail.com>
2021-03-19 15:25:55 +0100

    Created an agent instance variable in the bot.rb file and an API keys class in the api.rb file

---
+++ bin/bot.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# rubocop:disable Style/For
+# rubocop:disable Style/InfiniteLoop
+# rubocop:disable Lint/LiteralAsCondition
+
+require 'twitter'
+require 'open-uri'
+require_relative '../lib/api.rb'
+
+agent = API.new
+
+
+agent.client.update("My first bot tweet")
+
+# rubocop:enable Style/For
+# rubocop:enable Style/InfiniteLoop
+# rubocop:enable Lint/LiteralAsCondition
