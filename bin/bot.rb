a12fafbac4142b897ccf4894475a545786d50cca
Collins Tatang <makungongcollins@gmail.com>
2021-03-20 11:28:30 +0100

    Rubocop error fixed

--- bin/bot.rb
+++ bin/bot.rb
@@ -8,5 +8,5 @@ agent = API.new
 
 puts 'Search for any tweet with #rails and retweet again tagging the user name'
 agent.client.search('#rails').take(5).each do |tweet|
-    agent.client.update("@#{tweet.user.screen_name} Hey I love Rails too, what are your favorite blogs?")
+  agent.client.update("@#{tweet.user.screen_name} Hey I love Rails too, what are your favorite blogs?")
 end
