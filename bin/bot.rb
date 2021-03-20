53d467cbe7f25a6161e11f81d02f16dad796bcd7
Collins Tatang <makungongcollins@gmail.com>
2021-03-20 11:17:04 +0100

    Search method added to the bot.rb file

--- bin/bot.rb
+++ bin/bot.rb
@@ -6,4 +6,7 @@ require_relative '../lib/api.rb'
 
 agent = API.new
 
-agent.client.update('My first bot twee')
+puts 'Search for any tweet with #rails and retweet again tagging the user name'
+agent.client.search('#rails').take(5).each do |tweet|
+    agent.client.update("@#{tweet.user.screen_name} Hey I love Rails too, what are your favorite blogs?")
+end
